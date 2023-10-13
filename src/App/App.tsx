47b601e2161aa2a4cee18a2b68d676ec8698a83b
Alena <crimeaodessa2006Qgmail.com>
2023-10-13 19:21:04 +0200

    add reduser

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Theme from "../Theme/Theme";
+import Hits from "../components/Hits";
 
 const App = () => {
 	return (
 		<Theme>
 			<BrowserRouter>
 				<Header />
+				<Hits />
 			</BrowserRouter>
 		</Theme>
 	);
---
+++ src/redux/slices/productsSlice.tsx
@@ -0,0 +1,49 @@
+/* eslint-disable prettier/prettier */
+import { createSlice, PayloadAction, Dispatch } from "@reduxjs/toolkit";
+
+export interface Product {
+	id: number;
+	name: string;
+	price: string;
+	description: string;
+	collection: string;
+	imageUrls: [];
+}
+
+interface ProductsState {
+	products: Product[];
+}
+
+const initialState: ProductsState = {
+	products: [],
+};
+
+const productsSlice = createSlice({
+	name: "products",
+	initialState,
+	reducers: {
+		printProducts(state, action: PayloadAction<Product[]>){
+			state.products = action.payload;
+		},
+	},
+});
+
+export const { printProducts } = productsSlice.actions;
+
+export const getProductsAsync = (productsFile: string) => async(dispatch: Dispatch) => {
+	try {
+		const response = await fetch(`src/products/${productsFile}.json`);
+
+		if (response.ok){
+			const results: Product[] = await response.json();
+
+			dispatch(printProducts(results));
+		} else {
+			console.error("Error fetching products:", response.statusText);
+		}
+	} catch (error){
+		console.error("Error fetching products:", error);
+	}
+};
+
+export default productsSlice.reducer;
