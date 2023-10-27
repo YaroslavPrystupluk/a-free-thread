c11df792ac14612d24efa14726fc6d028fe333c1
Iaroslav05 <tyucha_05@ukr.net>
2023-10-27 18:56:48 +0300

    fix adaptive

--- src/components/SubHeader/index.tsx
+++ src/components/SubHeader/index.tsx
@@ -1,27 +1,46 @@
+import star from '../../images/icon/star.webp';
+import imgSubHeaderSmall from '../../images/Others/others_5.webp';
+import imgSubHeaderMedium from '../../images/Others/others_6.webp';
+import imgSubHeaderBig from '../../images/Others/others_7.webp';
+
 import {
 	SubHeaderWrapper,
 	OrnamentWrapper,
+	ContentWrapper,
 	SubHeaderTitle,
 	SubHeaderParagraph,
+	SubHeaderImage,
+	ImgSmall,
+	ImgMedium,
+	ImgBig,
+	SloganWrapper,
+	StarIcon,
+	Slogan,
 } from './styleSubHeader';
 
 const SubHeader = () => {
 	return (
 		<SubHeaderWrapper>
 			<OrnamentWrapper />
-			<section>
-				<SubHeaderTitle>Вільна нитка</SubHeaderTitle>
-				<SubHeaderParagraph>
-					Ми виробляємо етнічний одяг, сорочки, за індивідуальним замовленням, який ми відшиваємо і
-					вишиваємо за власним дизайном і кроєм. Маємо власну колекцію сорочок з кожного регіону
-					України.
-				</SubHeaderParagraph>
-				<SubHeaderParagraph>
-					Ми виробляємо етнічний одяг, сорочки, за індивідуальним замовленням, який ми відшиваємо і
-					вишиваємо за власним дизайном і кроєм. Маємо власну колекцію сорочок з кожного регіону
-					України.
-				</SubHeaderParagraph>
-			</section>
+			<ContentWrapper>
+				<div>
+					<SubHeaderTitle>Вільна нитка</SubHeaderTitle>
+					<SubHeaderParagraph>
+						Ми виробляємо етнічний одяг, сорочки, за індивідуальним замовленням, який ми відшиваємо
+						і вишиваємо за власним дизайном і кроєм. Маємо власну колекцію сорочок з кожного регіону
+						України.
+					</SubHeaderParagraph>
+				</div>
+				<SubHeaderImage>
+					<ImgBig src={imgSubHeaderBig} alt="ornament" />
+					<ImgMedium src={imgSubHeaderMedium} alt="ornament" />
+					<ImgSmall src={imgSubHeaderSmall} alt="ornament" />
+					<SloganWrapper>
+						<StarIcon src={star} alt="star icon" />
+						<Slogan>віднови вишиванку своєї бабусі.</Slogan>
+					</SloganWrapper>
+				</SubHeaderImage>
+			</ContentWrapper>
 		</SubHeaderWrapper>
 	);
 };
--- src/components/SubHeader/styleSubHeader.ts
+++ src/components/SubHeader/styleSubHeader.ts
@@ -5,7 +5,6 @@ const SubHeaderWrapper = styled.div`
 	display: flex;
 	gap: 30px;
 	justify-content: center;
-	align-items: center;
 	padding: 16px 0;
 	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
 		padding: 20px 0;
@@ -19,18 +18,25 @@ const SubHeaderWrapper = styled.div`
 const OrnamentWrapper = styled.section`
 	background: url(${ornamentImg}) 0 0 / contain repeat-y;
 	min-width: 67px;
-	min-height: 437px;
+	min-height: 100%;
 	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
 		min-width: 120px;
-		min-height: 360px;
 	}
 	@media (min-width: ${({ theme }) => `${theme.breakpoints.xl}px`}) {
 		min-width: 180px;
-		min-height: 540px;
 	}
 `;
 
-const SubHeaderTitle = styled.h1`
+const ContentWrapper = styled.section`
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-column-gap: 0px;
+		grid-row-gap: 0px;
+	}
+`;
+
+const SubHeaderTitle = styled.p`
 	font-family: ${({ theme }) => theme.fontFamily.title}, sans-serif;
 	font-size: 40px;
 	font-weight: 500;
@@ -46,8 +52,8 @@ const SubHeaderTitle = styled.h1`
 const SubHeaderParagraph = styled.p`
 	font-family: ${({ theme }) => theme.fontFamily.text}, sans-serif;
 	font-size: 12px;
-	line-height: 160%;
-	letter-spacing: 0.24px;
+	line-height: 173%;
+	margin: 32px 28px 0 0;
 	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
 		font-size: 14px;
 		line-height: 160%;
@@ -59,4 +65,74 @@ const SubHeaderParagraph = styled.p`
 	}
 `;
 
-export { SubHeaderWrapper, OrnamentWrapper, SubHeaderTitle, SubHeaderParagraph };
+const SubHeaderImage = styled.div`
+	position: relative;
+	margin: 13px 0 0 0;
+`;
+
+const ImgSmall = styled.img`
+	max-width: 16%;
+	height: auto;
+	position: absolute;
+	top: 60%;
+	left: 55%;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		max-width: 100%;
+	}
+`;
+const ImgMedium = styled.img`
+	max-width: 36%;
+	height: auto;
+	position: absolute;
+	left: 25%;
+	top: 25%;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		max-width: 100%;
+		left: 35%;
+		top: 35%;
+	}
+`;
+const ImgBig = styled.img`
+	max-width: 45%;
+	height: auto;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		max-width: 100%;
+	}
+`;
+
+const SloganWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+	position: absolute;
+	top: -16px;
+	left: 70%;
+`;
+
+const StarIcon = styled.img`
+	max-width: 16px;
+`;
+
+const Slogan = styled.span`
+	//width: 70px;
+	font-family: ${({ theme }) => theme.fontFamily.text}, sans-serif;
+	font-size: 10px;
+	line-height: 140%;
+	letter-spacing: 0.2px;
+	text-transform: uppercase;
+`;
+
+export {
+	SubHeaderWrapper,
+	OrnamentWrapper,
+	ContentWrapper,
+	SubHeaderTitle,
+	SubHeaderParagraph,
+	SubHeaderImage,
+	ImgSmall,
+	ImgMedium,
+	ImgBig,
+	SloganWrapper,
+	StarIcon,
+	Slogan,
+};
