d92ec501baa4ff04e094c3759ecc8ca2e0c267b8
Iaroslav05 <tyucha_05@ukr.net>
2023-11-03 19:41:24 +0200

    add header about

--- src/Theme/AboutTheme.ts
+++ src/Theme/AboutTheme.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
+import Grid from '@mui/material/Unstable_Grid2';
 
 const Container = styled.article`
 	max-width: ${({ theme }) => `${theme.breakpoints.xs}px`};
 	margin: 0 auto;
+	padding: 0 26px;
 	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
 		max-width: ${({ theme }) => `${theme.breakpoints.md}px`};
 	}
@@ -11,4 +13,93 @@ const Container = styled.article`
 	}
 `;
 
-export { Container };
+const TitleWrapper = styled.div`
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 67px 0 24px 0;
+`;
+
+const Title = styled.h1`
+	font-family: ${({ theme }) => theme.fontFamily.title}, sans-serif;
+	font-size: 20px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 160%;
+	letter-spacing: 0.4px;
+	text-transform: uppercase;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		font-size: 24px;
+		letter-spacing: 0.48px;
+	}
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.xl}px`}) {
+		font-size: 40px;
+		letter-spacing: 0.8px;
+	}
+`;
+
+const StarIcon = styled.img`
+	max-width: 16px;
+	position: absolute;
+	bottom: 65%;
+	left: 65%;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		max-width: 20px;
+		left: 57%;
+	}
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.xl}px`}) {
+		max-width: 24px;
+		left: 58%;
+	}
+`;
+
+const SquareTitle = styled.span`
+	min-width: 28px;
+	min-height: 28px;
+	position: absolute;
+	left: 28%;
+	bottom: 60%;
+	background: ${({ theme }) => theme.colors.primary};
+	opacity: 0.2;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		left: 40.5%;
+	}
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.xl}px`}) {
+		min-width: 40px;
+		min-height: 40px;
+		bottom: 50%;
+	}
+`;
+
+const GridAbout = styled(Grid)`
+	display: flex;
+	flex-direction: column;
+	&:nth-child(3) {
+		margin: 32px 0 0 0;
+	}
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.md}px`}) {
+		max-width: 50%;
+		&:nth-child(3) {
+			margin: 0;
+			order: 1;
+		}
+	}
+`;
+const AboutImage = styled.img`
+	max-width: 100%;
+	margin: 0 0 32px 0;
+	@media (min-width: ${({ theme }) => `${theme.breakpoints.sm}px`}) {
+		margin: 0;
+	}
+`;
+const AboutText = styled.p`
+	font-family: ${({ theme }) => theme.fontFamily.text}, sans-serif;
+	font-size: 12px;
+	line-height: 160%;
+	&:not(:last-child) {
+		padding: 0 0 25px 0;
+	}
+`;
+
+export { Container, TitleWrapper, Title, StarIcon, SquareTitle, GridAbout, AboutImage, AboutText };
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,42 +1,67 @@
 import Grid from '@mui/material/Unstable_Grid2';
-import Paper from '@mui/material/Paper';
-import { Container } from '../../Theme/AboutTheme';
+import {
+	Container,
+	TitleWrapper,
+	Title,
+	StarIcon,
+	SquareTitle,
+	GridAbout,
+	AboutImage,
+	AboutText,
+} from '../../Theme/AboutTheme';
 import aboutImageTop from '../../images/About/about_1.webp';
-import aboutImageButtom from '../../images/About/about.webp';
+import aboutImageButton from '../../images/About/about.webp';
+import star from '../../images/icon/star.webp';
 
 const About = () => {
 	return (
 		<Container>
-			<Grid container rowSpacing={1} columnSpacing={{ xs: 0, sm: 2, md: 3 }}>
-				<Grid xs={12} sm={6}>
-					<Paper>
-						<img src={aboutImageTop} alt="1" />
-					</Paper>
-				</Grid>
-				<Grid xs={12} sm={6}>
-					<Paper>
+			<TitleWrapper>
+				<Title>Про нас</Title>
+				<StarIcon src={star} alt="star" />
+				<SquareTitle />
+			</TitleWrapper>
+
+			<Grid
+				container
+				rowSpacing={{ xs: 0, sm: 7, md: 9.5 }}
+				columnSpacing={{ xs: 0, sm: 2.5, md: 3 }}
+			>
+				<GridAbout>
+					<AboutImage src={aboutImageTop} alt="needlework" />
+				</GridAbout>
+				<GridAbout>
+					<AboutText>
 						Ми – соціальний бізнес, що вирішує соціальну проблему навчання і працевлаштування жінок
-						з вразливих категорій, підтримку і відкритті власної швейної справи. Частину прибутку ми
-						спрямовуємо на фінансування соціальних проектів. Ми робимо особливий одяг за
-						індивідуальним замовленням, який може дозволити собі кожен, зберігаємо традицію
-						символічного вишивання.
-					</Paper>
-				</Grid>
-				<Grid xs={12} sm={6}>
-					<Paper>
-						<img src={aboutImageButtom} alt="1" />
-					</Paper>
-				</Grid>
-				<Grid xs={12} sm={6}>
-					<Paper>
+						з вразливих категорій, підтримку і відкритті власної швейної справи.
+					</AboutText>
+					<AboutText>
+						Частину прибутку ми спрямовуємо на фінансування соціальних проектів.
+					</AboutText>
+					<AboutText>
+						Ми робимо особливий одяг за індивідуальним замовленням, який може дозволити собі кожен,
+						зберігаємо традицію символічного вишивання.
+					</AboutText>
+				</GridAbout>
+				<GridAbout>
+					<AboutImage src={aboutImageButton} alt="seamstress" />
+				</GridAbout>
+				<GridAbout>
+					<AboutText>
 						Соціальне підприємство &quot;Вільна нитка&quot; було створене на базі притулку для
 						постраждалих від домашнього насильства в грудні 2021 року у вигляді швейно-вишивальної
-						майстерні, з сучасним обладнанням. В нашій майстерні жінки з вразливих категорій
-						навчаються на отримують оплачувані замовлення (багатодітні матері, пенсіонерки,
-						постраждалі від війни, домашнього насильства). Зараз наша майстерня розташована у м.
-						Вишгород, маємо плани по розширенні мережі майстерень.
-					</Paper>
-				</Grid>
+						майстерні, з сучасним обладнанням.
+					</AboutText>
+					<AboutText>
+						В нашій майстерні жінки з вразливих категорій навчаються на отримують оплачувані
+						замовлення (багатодітні матері, пенсіонерки, постраждалі від війни, домашнього
+						насильства).
+					</AboutText>
+					<AboutText>
+						Зараз наша майстерня розташована у м. Вишгород, маємо плани по розширенні мережі
+						майстерень.
+					</AboutText>
+				</GridAbout>
 			</Grid>
 		</Container>
 	);
